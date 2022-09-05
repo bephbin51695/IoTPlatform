36ef5e82054067a047041cb2ef77308df6ca3467
bephbin <bephbin51695@gmail.com>
2022-09-05 08:58:27 +0800

    add cookie

--- src/main/java/com/yzk/interceptor/PrivilegeInterceptor.java
+++ src/main/java/com/yzk/interceptor/PrivilegeInterceptor.java
@@ -3,18 +3,33 @@ package com.yzk.interceptor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yzk.domain.R;
 import com.yzk.domain.User;
+import com.yzk.util.LocalStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class PrivilegeInterceptor implements HandlerInterceptor{
+@Component
+public class PrivilegeInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private LocalStorage localStorage;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
-        if(user==null){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName()) && localStorage.getLoginCookieMap().get(user.getUsername()).getValue().equals(cookie.getValue())) {
+                return true;
+            }
+        }
+        if (user == null) {
             ObjectMapper mapper = new ObjectMapper();
             String json = mapper.writeValueAsString(new R("未登录"));
             response.setContentType("application/json;charset=utf-8");
